8cf20427f2c1850a8bb2f2766d37d4ec9db297cc
tuan204-dev <tuan204.dev@gmail.com>
2023-06-24 10:47:01 +0700

    add NotFound Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import {
-  Route,
-  Routes
-} from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Split from 'react-split'
 import styles from './App.module.scss'
 import Sidebar from './components/Sidebar/Sidebar'
@@ -10,8 +8,17 @@ import Artist from './pages/Artist/Artist'
 import Home from './pages/Home/Home'
 import Search from './pages/Search/Search'
 import './resizable.scss'
+import NotFound from './components/NotFound/NotFound'
 
 function App() {
+  const { pathname } = useLocation()
+  const [showSidebar, setShowSidebar] = useState<boolean>(true)
+
+  useEffect(() => {
+    setShowSidebar(['/', '/search', 'artist'].includes(pathname))
+  }, [pathname])
+  console.log(showSidebar, pathname)
+
   return (
     <div className={styles.app}>
       <Split
@@ -19,20 +26,21 @@ function App() {
         minSize={280}
         maxSize={[600]}
         sizes={[20, 80]}
-        // gutterAlign={'start'}
-        // sizes={[20]}
-        // gutterSize={8}
         className={styles.split}
       >
-        <Sidebar />
+        {showSidebar && <Sidebar />}
         <MainLayoutProvider>
-          <div className={styles.main}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/artist" element={<Artist />} />
-            </Routes>
-          </div>
+          {showSidebar ? (
+            <div className={styles.main}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/artist" element={<Artist />} />
+              </Routes>
+            </div>
+          ) : (
+            <NotFound />
+          )}
         </MainLayoutProvider>
       </Split>
     </div>
---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './NotFound.module.scss'
+import classNames from 'classnames/bind'
+import logoImage from '@/assets/image/logo/logo.svg'
+import { Link } from 'react-router-dom'
+
+const cx = classNames.bind(styles)
+
+const NotFound: React.FC = () => {
+  return (
+    <div className={cx('wrapper')}>
+      <div
+        style={{ backgroundImage: `url(${logoImage})` }}
+        className={cx('logo')}
+      ></div>
+      <div className={cx('body')}>
+        <h2 className={cx('body__heading')}>Page not found</h2>
+        <p className={cx('body__sub-heading')}>
+          We can’t seem to find the page you are looking for.
+        </p>
+        <a href="/" className={cx('body__home-btn')}>
+          Home
+        </a>
+        <a
+          className={cx('body__help')}
+          href="https://www.facebook.com/tuan204.dev"
+          target="_blank"
+        >
+          Help
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
