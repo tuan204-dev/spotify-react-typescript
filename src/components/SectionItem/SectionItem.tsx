ea45cc16f7ee512bdde40163bab9bcb1a7e937ee
tuan204-dev <tuan204.dev@gmail.com>
2023-06-25 22:19:17 +0700

    fix some bug

--- src/components/SectionItem/SectionItem.tsx
+++ src/components/SectionItem/SectionItem.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './SectionItem.module.scss'
 import classNames from 'classnames/bind'
 import { SectionItemI } from '../../../types'
+import { TbPlayerPlayFilled } from 'react-icons/tb'
 
 const cx = classNames.bind(styles)
 
@@ -9,13 +10,19 @@ const SectionItem: React.FC<SectionItemI> = ({
   title,
   name,
   imageUrl,
-  columnWidth,
-  id,
+
 }) => {
   return (
     <div className={cx('wrapper')}>
       <div className={cx('img')}>
         <img src={imageUrl} alt={title || name} />
+        <button
+          className={cx({
+            'play-btn': true,
+          })}
+        >
+          <TbPlayerPlayFilled className={cx('play-btn-child')} />
+        </button>
       </div>
       <div className={cx('body')}>
         <h3 className={cx('heading')}>{title || name}</h3>
